073ced92678497b8dfa2c6232ade8fb7fa9281d5
Heath <43174742+heathguy@users.noreply.github.com>
2018-09-19 09:30:01 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,247 @@
+var tiles = [];
+var wholeSnake = [];
+var snakeDir;
+var food;
+
+var mdir;
+
+var rows, cols;
+var tileSize = 10;
+
+var gameOver;
+var gamePaused;
+
+var gameSpeed = 10;
+
+var gameScore = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  background(51);
+  frameRate(gameSpeed);
+
+  rows = height / tileSize;
+  cols = width / tileSize;
+
+  // for (var i = 0; i < cols; i++) {
+  //   tiles[i] = [];
+  //   for (var j = 0; j < rows; j++) {
+  //     tiles[i][j] = new Tile(i, j, tileSize, 0);
+  //   }
+  // }
+
+  //console.log("Number Rows,Cols: " + rows + ", " + cols);
+
+  fill(255, 0, 0);
+  var startX;
+  var startY;
+  for (var i = 0; i < cols; i++) {
+    startX = i * tileSize;
+    for (var j = 0; j < rows; j++) {
+      startY = j * tileSize;
+      //rect(startX, startY, startX + tileSize, startY + tileSize);
+    }
+    j = j * tileSize;
+  }
+
+  // start snake
+  snakeDir = createVector(tileSize, 0);
+  mdir = 3;
+  sx = 50;
+  sy = floor(width / 2);
+
+  var snakeHead = new Snake(sx, sy, tileSize, 1, snakeDir.x, snakeDir.y);
+  snakeHead.movementDir = mdir; // set initial direction to the right
+  wholeSnake.push(snakeHead);
+
+  gameOver = false;
+  gamePaused = true;
+
+  food = createFood();
+}
+
+function getRandomInt(min, max) {
+  return floor(random() * (max - min + 1) + min);
+}
+
+function getRandom10(min, max) {
+  return getRandomInt(min / 10, max / 10) * 10;
+}
+
+function createFood() {
+  // get a random point on the board
+  var fx = getRandom10(10, width - tileSize);
+  var fy = getRandom10(10, height - tileSize);
+
+  var fpos = createVector(fx, fy);
+  console.log(fx + ", " + fy);
+
+  return fpos;
+}
+
+function mousePressed() {
+  // do nothing
+  if (gamePaused) {
+    gamePaused = false;
+  } else {
+    gamePaused = true;
+  }
+}
+
+function keyPressed() {
+  // 0; up, 1: down, 2: left, 3: right
+  //mdir = 0;
+  switch (keyCode) {
+    case RIGHT_ARROW:
+      snakeDir.x = tileSize;
+      snakeDir.y = 0;
+      mdir = 3;
+      break;
+    case DOWN_ARROW:
+      snakeDir.x = 0;
+      snakeDir.y = tileSize;
+      mdir = 1;
+      break;
+    case LEFT_ARROW:
+      snakeDir.x = (tileSize * -1);
+      snakeDir.y = 0;
+      mdir = 2;
+      break;
+    case UP_ARROW:
+      snakeDir.x = 0;
+      snakeDir.y = (tileSize * -1);
+      mdir = 0;
+      break;
+    default:
+      gamePaused = false;
+  }
+
+  // wholeSnake[0].moveHead(snakeDir, mdir);
+  // if (wholeSnake.length > 1) {
+  //   for (var i = 1; i < wholeSnake.length; i++) {
+  //     wholeSnake[i].moveBody(snakeDir, mdir, wholeSnake[i - 1]);
+  //     // check for collision with the snake body
+  //     if (wholeSnake[0].pos.x == wholeSnake[i].pos.x && wholeSnake[0].pos.y ==
+  //       wholeSnake[i].pos.y) {
+  //       gameOver = true;
+  //       break;
+  //     }
+  //   }
+  // }
+  //
+  // // check edge collisions
+  // // if hit any edge or any part of snake body game is over
+  // if (wholeSnake[0].pos.x < 0 || wholeSnake[0].pos.x + tileSize > width) {
+  //   gameOver = true;
+  // } else if (wholeSnake[0].pos.y < 0 || wholeSnake[0].pos.y + tileSize > height) {
+  //   gameOver = true;
+  // } else {}
+
+}
+
+function draw() {
+  background(51);
+
+  if (!gameOver) {
+    //fill(255, 0, 0);
+    // fill(51);
+    // stroke(0);
+    // var startX;
+    // var startY;
+    // for (var i = 0; i < cols; i++) {
+    //   startX = i * tileSize;
+    //   for (var j = 0; j < rows; j++) {
+    //     startY = j * tileSize;
+    //     rect(startX, startY, startX + tileSize, startY + tileSize);
+    //   }
+    //   j = j * tileSize;
+    // }
+
+    // wholeSnake[0].showHead();
+    // for (var i = 1; i < wholeSnake.length; i++) {
+    //   wholeSnake[i].showBody();
+    // }
+    // if (wholeSnake.length > 1) {
+    //   wholeSnake[wholeSnake.length - 1].showTail(wholeSnake[wholeSnake.length -
+    //       2]
+    //     .movementDir);
+    // }
+    // do not move the snake if the game is paused
+    if (!gamePaused) {
+      wholeSnake[0].moveHead(snakeDir, mdir);
+      if (wholeSnake.length > 1) {
+        for (var i = 1; i < wholeSnake.length; i++) {
+          wholeSnake[i].moveBody(snakeDir, mdir, wholeSnake[i - 1]);
+          // check for collision with the snake body
+          if (wholeSnake[0].pos.x == wholeSnake[i].pos.x && wholeSnake[0]
+            .pos.y ==
+            wholeSnake[i].pos.y) {
+            gameOver = true;
+            break;
+          }
+        }
+      }
+    }
+    // show the food piece
+    fill(200, 200, 100);
+    rect(food.x, food.y, tileSize, tileSize);
+
+
+    wholeSnake[0].showHead();
+    for (var i = 1; i < wholeSnake.length; i++) {
+      wholeSnake[i].showBody();
+    }
+    if (wholeSnake.length > 1) {
+      wholeSnake[wholeSnake.length - 1].showTail(wholeSnake[wholeSnake.length -
+          2]
+        .movementDir);
+    }
+
+    // check eat food
+    if (wholeSnake[0].pos.x == food.x && wholeSnake[0].pos.y == food.y) {
+      //console.log("GOT FOOD!");
+      var snakePiece;
+      // grow the snake
+      //add based on current movement direction mdir
+      switch (mdir) {
+        case 0: // moving up
+          snakePiece = new Snake(wholeSnake[wholeSnake.length - 1].pos.x,
+            wholeSnake[wholeSnake.length - 1].pos.y + tileSize, tileSize, 2,
+            snakeDir.x, snakeDir.y);
+          break;
+        case 1: // moving down
+          snakePiece = new Snake(wholeSnake[wholeSnake.length - 1].pos.x,
+            wholeSnake[wholeSnake.length - 1].pos.y - tileSize, tileSize, 2,
+            snakeDir.x, snakeDir.y);
+          break;
+        case 2: // movking left
+          snakePiece = new Snake(wholeSnake[wholeSnake.length - 1].pos.x +
+            tileSize,
+            wholeSnake[wholeSnake.length - 1].pos.y, tileSize, 2,
+            snakeDir.x, snakeDir.y);
+          break;
+        case 3: // moving right
+          snakePiece = new Snake(wholeSnake[wholeSnake.length - 1].pos.x -
+            tileSize,
+            wholeSnake[wholeSnake.length - 1].pos.y, tileSize, 2,
+            snakeDir.x, snakeDir.y);
+          break;
+        default:
+      }
+      wholeSnake.push(snakePiece);
+    }
+
+    // check edge collisions
+    // if hit any edge or any part of snake body game is over
+    if (wholeSnake[0].pos.x < 0 || wholeSnake[0].pos.x + tileSize > width) {
+      gameOver = true;
+    } else if (wholeSnake[0].pos.y < 0 || wholeSnake[0].pos.y + tileSize >
+      height) {
+      gameOver = true;
+    } else {}
+
+  } else {
+    noLoop();
+    console.log("GAME OVER!");
+  }
+}
---
+++ snake.js
@@ -0,0 +1,86 @@
+function Snake(x, y, scl, t, px, py) {
+  this.pos = createVector(x, y);
+  this.type = t;
+  this.size = scl;
+  //if (t != 1) {
+  this.parentpos = createVector(px, py);
+  this.movementDir = 0; // 0; up, 1: down, 2: left, 3: right
+  //}
+
+  this.showBody = function() {
+
+    fill(150, 100, 150);
+    rect(this.pos.x, this.pos.y, this.size, this.size);
+
+  }
+
+  // draw a rounded rectangle for the head
+  this.showHead = function() {
+    fill(0, 255, 0);
+    var headRadius = 30;
+    switch (this.movementDir) {
+      case 0: // moving up
+        rect(this.pos.x, this.pos.y, this.size, this.size, headRadius,
+          headRadius, 0, 0);
+        break;
+      case 1: // moving down
+        rect(this.pos.x, this.pos.y, this.size, this.size, 0, 0, headRadius,
+          headRadius);
+        break;
+      case 2: // movking left
+        rect(this.pos.x, this.pos.y, this.size, this.size, headRadius, 0, 0,
+          headRadius);
+        break;
+      case 3: // moving right
+        rect(this.pos.x, this.pos.y, this.size, this.size, 0, headRadius,
+          headRadius, 0);
+        break;
+      default:
+    }
+  }
+
+  // draw a triangle for the tail based off the parent's movement direction
+  this.showTail = function(parentmdir) {
+    fill(0, 255, 0);
+    switch (parentmdir) {
+      case 0: // moving up
+        triangle(this.pos.x, this.pos.y,
+          this.pos.x + tileSize, this.pos.y + tileSize,
+          this.pos.x + tileSize * 0.5, this.pos.y + tileSize);
+        break;
+      case 1: // moving down
+        triangle(this.pos.x + tileSize * 0.5, this.pos.y,
+          this.pos.x + tileSize, this.pos.y + tileSize,
+          this.pos.x, this.pos.y + tileSize);
+        break;
+      case 2: // movking left
+        triangle(this.pos.x, this.pos.y,
+          this.pos.x + tileSize, this.pos.y + tileSize * 0.5,
+          this.pos.x, this.pos.y + tileSize);
+        break;
+      case 3: // moving right
+        triangle(this.pos.x, this.pos.y + tileSize * 0.5,
+          this.pos.x + tileSize, this.pos.y,
+          this.pos.x + tileSize, this.pos.y + tileSize);
+        break;
+      default:
+    }
+  }
+
+  this.moveHead = function(dir, mdir) {
+    this.pos.x += dir.x;
+    this.pos.y += dir.y;
+
+    this.movementDir = mdir;
+  }
+
+  this.moveBody = function(dir, mdir, parent) {
+    this.movementDir = mdir;
+
+    this.pos.x = this.parentpos.x;
+    this.pos.y = this.parentpos.y;
+
+    this.parentpos.x = parent.pos.x;
+    this.parentpos.y = parent.pos.y;
+  }
+}
