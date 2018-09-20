1f620368b2f6ec3fe084f458fe408cfad5b6fb91
Heath <43174742+heathguy@users.noreply.github.com>
2018-09-20 09:55:17 -0400

    Fixed #3 By changing how the snake is moved
    Get pos of head of snake and then move it. Loop through the rest of the snake setting the current position to the previous.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+/**
+ Snake Game Clone
+**/
+
 var tiles = [];
 var wholeSnake = [];
 var snakeDir;
@@ -6,13 +10,11 @@ var food;
 var mdir;
 
 var rows, cols;
-var tileSize = 10;
+var tileSize = 20;
 
 var gameOver;
 var gamePaused;
-
 var gameSpeed;
-
 var gameScore = 0;
 
 function setup() {
@@ -20,40 +22,19 @@ function setup() {
   createCanvas(400, 400);
   background(51);
 	
-	gameSpeed = width/tileSize*0.5
+	gameSpeed = width/tileSize*0.25
   frameRate(gameSpeed);
 
   rows = height / tileSize;
   cols = width / tileSize;
 
-  // for (var i = 0; i < cols; i++) {
-  //   tiles[i] = [];
-  //   for (var j = 0; j < rows; j++) {
-  //     tiles[i][j] = new Tile(i, j, tileSize, 0);
-  //   }
-  // }
-
-  //console.log("Number Rows,Cols: " + rows + ", " + cols);
-
-  fill(255, 0, 0);
-  var startX;
-  var startY;
-  for (var i = 0; i < cols; i++) {
-    startX = i * tileSize;
-    for (var j = 0; j < rows; j++) {
-      startY = j * tileSize;
-      //rect(startX, startY, startX + tileSize, startY + tileSize);
-    }
-    j = j * tileSize;
-  }
-
-  // start snake
+  // start snake moving to the right
   snakeDir = createVector(tileSize, 0);
   mdir = 3;
-  sx = 50;
+  sx = tileSize;
   sy = floor(width / 2);
 
-  var snakeHead = new Snake(sx, sy, tileSize, 1, snakeDir.x, snakeDir.y);
+  var snakeHead = new Snake(sx, sy, tileSize, 1, sx+tileSize, sy);
   snakeHead.movementDir = mdir; // set initial direction to the right
   wholeSnake.push(snakeHead);
 
@@ -67,19 +48,12 @@ function getRandomInt(min, max) {
   return floor(random() * (max - min + 1) + min);
 }
 
-function getRandom10(min, max) {
-  return getRandomInt(min / 10, max / 10) * 10;
-}
-
 function getRandomTileSize(min, max) {
   return getRandomInt(min / tileSize, max / tileSize) * tileSize;
 }
 
 function createFood() {
   // get a random point on the board
-  //var fx = getRandom10(10, width - tileSize);
-  //var fy = getRandom10(10, height - tileSize);
-	
   var fx = getRandomTileSize(tileSize, width - tileSize);
   var fy = getRandomTileSize(tileSize, height - tileSize);
 
@@ -125,28 +99,6 @@ function keyPressed() {
     default:
       gamePaused = false;
   }
-
-  // wholeSnake[0].moveHead(snakeDir, mdir);
-  // if (wholeSnake.length > 1) {
-  //   for (var i = 1; i < wholeSnake.length; i++) {
-  //     wholeSnake[i].moveBody(snakeDir, mdir, wholeSnake[i - 1]);
-  //     // check for collision with the snake body
-  //     if (wholeSnake[0].pos.x == wholeSnake[i].pos.x && wholeSnake[0].pos.y ==
-  //       wholeSnake[i].pos.y) {
-  //       gameOver = true;
-  //       break;
-  //     }
-  //   }
-  // }
-  //
-  // // check edge collisions
-  // // if hit any edge or any part of snake body game is over
-  // if (wholeSnake[0].pos.x < 0 || wholeSnake[0].pos.x + tileSize > width) {
-  //   gameOver = true;
-  // } else if (wholeSnake[0].pos.y < 0 || wholeSnake[0].pos.y + tileSize > height) {
-  //   gameOver = true;
-  // } else {}
-
 }
 
 function draw() {
@@ -167,30 +119,27 @@ function draw() {
     //   j = j * tileSize;
     // }
 
-    // wholeSnake[0].showHead();
-    // for (var i = 1; i < wholeSnake.length; i++) {
-    //   wholeSnake[i].showBody();
-    // }
-    // if (wholeSnake.length > 1) {
-    //   wholeSnake[wholeSnake.length - 1].showTail(wholeSnake[wholeSnake.length -
-    //       2]
-    //     .movementDir);
-    // }
     // do not move the snake if the game is paused
     if (!gamePaused) {
-      wholeSnake[0].moveHead(snakeDir, mdir);
-      if (wholeSnake.length > 1) {
-        for (var i = 1; i < wholeSnake.length; i++) {
-          wholeSnake[i].moveBody(snakeDir, mdir, wholeSnake[i - 1]);
-          // check for collision with the snake body
-          if (wholeSnake[0].pos.x == wholeSnake[i].pos.x && wholeSnake[0]
-            .pos.y ==
-            wholeSnake[i].pos.y) {
+			var nextX = wholeSnake[0].pos.x; // store head's current pos
+			var nextY = wholeSnake[0].pos.y;
+			var currX = 0;
+			var currY = 0;
+			wholeSnake[0].moveHead(snakeDir, mdir); // move head of snake
+			//move rest of body
+			for (var i = 1; i < wholeSnake.length; i++) {
+				currX = wholeSnake[i].pos.x;
+				currY = wholeSnake[i].pos.y;
+				wholeSnake[i].pos.x = nextX;
+				wholeSnake[i].pos.y = nextY;
+				nextX = currX;
+				nextY = currY;
+				// check for collision with the snake body
+				if (wholeSnake[0].pos.x == wholeSnake[i].pos.x && wholeSnake[0].pos.y == wholeSnake[i].pos.y) {
             gameOver = true;
             break;
           }
-        }
-      }
+			}
     }
     // show the food piece
     fill(200, 200, 100);
@@ -198,8 +147,8 @@ function draw() {
 
 
     wholeSnake[0].showHead();
-    for (var i = 1; i < wholeSnake.length; i++) {
-      wholeSnake[i].showBody();
+    for (var j = 1; j < wholeSnake.length; j++) {
+      wholeSnake[j].showBody();
     }
     if (wholeSnake.length > 1) {
       wholeSnake[wholeSnake.length - 1].showTail(wholeSnake[wholeSnake.length -
